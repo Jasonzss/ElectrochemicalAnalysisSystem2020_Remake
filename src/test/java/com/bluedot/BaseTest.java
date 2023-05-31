6cb9266cfa3dd11955df3e536c69971963004098
Jasonzss <2418972236@qq.com>
2023-05-31 11:24:25 +0800

    添加了一些基础配置和依赖

---
+++ src/test/java/com/bluedot/BaseTest.java
@@ -0,0 +1,13 @@
+package com.bluedot;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @Author Jason
+ * @CreationDate 2023/05/30 - 18:35
+ * @Description ：基础测试类，保存可以复用很多测试代码
+ */
+public class BaseTest {
+    public static final Logger log = LoggerFactory.getLogger(BaseTest.class);
+}
