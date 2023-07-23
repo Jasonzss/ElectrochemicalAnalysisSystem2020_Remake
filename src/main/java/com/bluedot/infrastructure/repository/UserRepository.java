87ebca19109478962672b3f48d4908a400229f20
Jasonzss <2418972236@qq.com>
2023-07-24 00:58:00 +0800

    优化导入

--- src/main/java/com/bluedot/infrastructure/repository/UserRepository.java
+++ src/main/java/com/bluedot/infrastructure/repository/UserRepository.java
@@ -5,8 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
-import java.util.List;
-
 /**
  * @Author Jason
  * @CreationDate 2023/05/28 - 14:33
