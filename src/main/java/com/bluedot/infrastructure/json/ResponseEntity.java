8dda68f02f4c2951666e37494b738123de2707a9
Jasonzss <2418972236@qq.com>
2023-07-26 23:54:45 +0800

    基础模块提供json功能配置

---
+++ src/main/java/com/bluedot/infrastructure/json/ResponseEntity.java
@@ -0,0 +1,8 @@
+package com.bluedot.infrastructure.json;
+
+/**
+ * @author Jason
+ * @since 2023/07/26 - 23:17
+ */
+public interface ResponseEntity {
+}
---
+++ src/main/java/com/bluedot/infrastructure/json/ResponseGsonFactoryBean.java
@@ -0,0 +1,133 @@
+package com.bluedot.infrastructure.json;
+
+import com.bluedot.domain.process.model.Point;
+import com.bluedot.infrastructure.repository.enumeration.UserStatus;
+import com.bluedot.infrastructure.utils.Quantity;
+import com.google.gson.*;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.FactoryBean;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+/**
+ * @author Jason
+ * @since 2023/07/26 - 21:25
+ *
+ * 对响应返回的对象进行json化的FactoryBean
+ */
+@Component
+public class ResponseGsonFactoryBean implements FactoryBean<Gson> {
+    /**
+     * 添加有Quantity、Point重写逻辑的gson
+     */
+    @Override
+    public Gson getObject() {
+        return new GsonBuilder()
+               .registerTypeAdapter(Quantity.class, new QuantityTypeAdapter())
+               .registerTypeAdapter(Point.class, new PointTypeAdapter())
+               .registerTypeAdapter(UserStatus.class, new UserStatusTypeAdapter())
+               .setExclusionStrategies(new ResponseIgnoreExclusionStrategies())
+               .create();
+    }
+
+    @Override
+    public Class<?> getObjectType() {
+        return Gson.class;
+    }
+
+
+
+    private static class QuantityTypeAdapter extends TypeAdapter<Quantity> {
+        /**
+         * 两Quantity类输出为Json格式
+         * @param out Json输出
+         * @param value 需要输出的Quantity
+         * @throws IOException 输出IO异常
+         */
+        @Override
+        public void write(JsonWriter out, Quantity value) throws IOException {
+            if(value != null){
+                out.value(value.toString());
+            }else {
+                //没有值的时候不输出为json
+                out.nullValue();
+            }
+        }
+
+        @Override
+        public Quantity read(JsonReader in) throws IOException {
+            String s = in.nextString();
+            if(StringUtils.isEmpty(s)){
+                return new Quantity(null , null);
+            }else {
+                return new Quantity(s);
+            }
+        }
+    }
+
+
+    private static class PointTypeAdapter extends TypeAdapter<Point> {
+        @Override
+        public void write(JsonWriter out, Point value) throws IOException {
+            if(value != null){
+                String s = value.getX().toString()+","+value.getY().toString();
+                out.value(s);
+            }else {
+                //没有值的时候不输出为json
+                out.nullValue();
+            }
+        }
+
+        @Override
+        public Point read(JsonReader in) throws IOException {
+            String s = in.nextString();
+            if(StringUtils.isEmpty(s)){
+                return new Point(new Quantity(null, null), new Quantity(null, null));
+            }else {
+                String[] split = s.split(",");
+                return new Point(new Quantity(split[0]), new Quantity(split[1]));
+            }
+        }
+    }
+
+    private static class UserStatusTypeAdapter extends TypeAdapter<UserStatus>{
+        @Override
+        public void write(JsonWriter out, UserStatus value) throws IOException {
+            if(value != null){
+                String s = value.getStatus();
+                out.value(s);
+            }else {
+                //没有值的时候不输出为json
+                out.nullValue();
+            }
+        }
+
+        @Override
+        public UserStatus read(JsonReader in) throws IOException {
+            String s = in.nextString();
+            if(StringUtils.isEmpty(s)){
+                return null;
+            }else {
+                return UserStatus.valueOf(s);
+            }
+        }
+    }
+
+    /**
+     * 带有 {@link ResponseIgnore} 注解的字段将会被忽略
+     */
+    private static class ResponseIgnoreExclusionStrategies implements ExclusionStrategy {
+        @Override
+        public boolean shouldSkipField(FieldAttributes f) {
+            return f.getAnnotation(ResponseIgnore.class) != null;
+        }
+
+        @Override
+        public boolean shouldSkipClass(Class<?> clazz) {
+            return false;
+        }
+    }
+}
