5d9590a080fa759b8076d0c3de7bb3960db2900b
Jasonzss <2418972236@qq.com>
2023-07-27 22:00:16 +0800

    完善

--- src/main/java/com/bluedot/application/CaptchaDiagramService.java
+++ src/main/java/com/bluedot/application/CaptchaDiagramService.java
@@ -2,6 +2,7 @@ package com.bluedot.application;
 
 import cn.hutool.captcha.CaptchaUtil;
 import cn.hutool.captcha.LineCaptcha;
+import org.springframework.beans.factory.FactoryBean;
 import org.springframework.stereotype.Component;
 
 import javax.inject.Singleton;
@@ -14,15 +15,21 @@ import java.util.*;
  */
 @Singleton
 @Component
-public class CaptchaDiagramService {
+public class CaptchaDiagramService implements FactoryBean<CaptchaDiagramService> {
     /**
      * 字体只显示大写，去掉了1,0,I,O几个容易混淆的字符
      */
     public static final String VERIFY_CODES = "23456789ABCDEFGHJKLMNPQRSTUVWXYZ";
     public static final Integer CODE_LENGTH = 6;
 
+    /**
+     * 验证码集合：key为设置在客户端的cookie，用于唯一标识；value是对应客户端的验证码
+     */
     private static final Map<UUID,String> captchaCodes = new HashMap<>();
 
+    /**
+     * 通过验证的设备的cookie
+     */
     Set<UUID> captchaPassed = new HashSet<>();
 
     public BufferedImage getCaptchaDiagram(UUID uuid){
@@ -66,5 +73,46 @@ public class CaptchaDiagramService {
             verifyCode.append(VERIFY_CODES.charAt(rand.nextInt(codesLen - 1)));
         }
         return verifyCode.toString();
+    }
+
+    /**
+     * 仅供测试使用
+     *
+     * @param uuid 验证码对应的唯一id
+     * @return 验证码
+     */
+    public String getVerifyCode(String uuid){
+        return captchaCodes.get(UUID.fromString(uuid));
+    }
+
+
+    //-----------------------FactoryBean实现--------------------------------
+
+    @Override
+    public CaptchaDiagramService getObject() throws Exception {
+        return getCaptchaDiagramService();
+    }
+
+    @Override
+    public Class<?> getObjectType() {
+        return CaptchaDiagramService.class;
+    }
+
+    @Override
+    public boolean isSingleton() {
+        return true;
+    }
+
+
+    //-----------------------实现单例模式--------------------------------
+
+    private static class CaptchaDiagramServiceHolder {
+        private static final CaptchaDiagramService INSTANCE = new CaptchaDiagramService();
+    }
+
+    private CaptchaDiagramService (){}
+
+    public static final CaptchaDiagramService getCaptchaDiagramService() {
+        return CaptchaDiagramServiceHolder.INSTANCE;
     }
 }
